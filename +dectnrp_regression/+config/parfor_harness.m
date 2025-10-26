3c04af00dc2bf6ae6799d83851675c8d89c5a0d7
Maxim Penner <maxim.penner.dev@gmail.com>
2025-10-26 16:06:17 +0100

    add inner_repetitions

--- +dectnrp_regression/+config/parfor_harness.m
+++ +dectnrp_regression/+config/parfor_harness.m
@@ -1,5 +1,8 @@
 function [] = parfor_harness(range, func)
 
+    assert(isscalar(range.inner_repetitions));
+    assert(range.inner_repetitions >= 1);
+
     % test a large selection of packet configurations
     for u = range.u_vec
         %for b_vec_idx = 1:numel(b_vec)
@@ -15,26 +18,28 @@ function [] = parfor_harness(range, func)
                                         for PLCF_type = range.PLCF_type_vec
                                             for rv = range.rv_vec
                                                 for network_id = range.network_id_vec
-
-                                                    % configurations are initialized with exemplary values
-                                                    config = dectnrp_tx.config_t();
-
-                                                    % overwrite
-                                                    config.u = u;
-                                                    config.b = b;
-                                                    config.PacketLengthType = PacketLengthType;
-                                                    config.PacketLength = PacketLength;
-                                                    config.tm_mode_0_to_11 = tm_mode_0_to_11;
-                                                    config.mcs_index = mcs_index;
-                                                    config.Z = Z;
-                                                    config.oversampling = oversampling;
-                                                    config.codebook_index = codebook_index;
-                                                    config.PLCF_type = PLCF_type;
-                                                    config.rv = rv;
-                                                    config.network_id = de2bi(network_id,32,'left-msb');
-                                                    config.verbosity = range.verbosity;
-                        
-                                                    func(config);
+                                                    for inner_repetition = 1:range.inner_repetitions
+    
+                                                        % configurations are initialized with exemplary values
+                                                        config = dectnrp_tx.config_t();
+    
+                                                        % overwrite
+                                                        config.u = u;
+                                                        config.b = b;
+                                                        config.PacketLengthType = PacketLengthType;
+                                                        config.PacketLength = PacketLength;
+                                                        config.tm_mode_0_to_11 = tm_mode_0_to_11;
+                                                        config.mcs_index = mcs_index;
+                                                        config.Z = Z;
+                                                        config.oversampling = oversampling;
+                                                        config.codebook_index = codebook_index;
+                                                        config.PLCF_type = PLCF_type;
+                                                        config.rv = rv;
+                                                        config.network_id = de2bi(network_id,32,'left-msb');
+                                                        config.verbosity = range.verbosity;
+                            
+                                                        func(config);
+                                                    end
                                                 end
                                             end
                                         end
